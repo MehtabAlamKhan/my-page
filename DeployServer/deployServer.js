5153eaae2424ba80a644ca8fe7f002ba2730538a
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-11-01 20:27:35 +0530

    scripts

--- DeployServer/deployServer.js
+++ DeployServer/deployServer.js
@@ -1,3 +1,13 @@
 import http from "node:http";
 
-const deployServer = http.createServer((req, res) => {}).listen(8080);
+const deployServer = http
+  .createServer((req, res) => {
+    let msg;
+    req.on("data", (chunk) => {
+      msg += chunk.toString("utf-8");
+    });
+    req.on("end", () => {
+      console.log(msg);
+    });
+  })
+  .listen(8080);
